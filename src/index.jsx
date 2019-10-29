f1c066ab2d67c1f1ab97eb244fbf26f60d465637
Zach Smith <zachsmith.sa@gmail.com>
2019-10-29 10:31:48 +0200

    added cell and grid, removed dist

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import './index.scss'
 import React from 'react'
 import { render } from "react-dom"
 import WebFontLoader from 'webfontloader'
-import { Card, CardTitle, CardText, Slider } from 'react-md';
+import { Card, CardTitle, CardText, Slider, Grid, Cell } from 'react-md';
 
 // Load the fonts
 WebFontLoader.load({
@@ -12,16 +12,20 @@ WebFontLoader.load({
 })
 
 const App = () => (
-  <Card>
-    <CardTitle title="Using CardTitle" subtitle="With CardText" />
-    <CardText>
-      <p>
-        The <code>CardText</code> component is really just useful for displaying any
-        content with some additional padding.
-      </p>
-      <Slider id="example-card-slider" />
-    </CardText>
-  </Card>
+  <Grid>
+    <Cell>
+      <Card>
+        <CardTitle title="Using CardTitle" subtitle="With CardText" />
+        <CardText>
+          <p>
+            The <code>CardText</code> component is really just useful for displaying any
+            content with some additional padding.
+          </p>
+          <Slider id="example-card-slider" />
+        </CardText>
+      </Card>
+    </Cell>
+  </Grid>
 )
 
 render(
